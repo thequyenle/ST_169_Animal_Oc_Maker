22f870aaee435807db8651fbd12266c9850453ab
thequyenle <quyenle26082001@gmail.com>
2025-10-25 16:57:05 +0700

    fix

--- app/src/main/java/com/example/st181_halloween_maker/core/service/ApiService.kt
+++ app/src/main/java/com/example/st181_halloween_maker/core/service/ApiService.kt
@@ -3,6 +3,6 @@ import com.example.st181_halloween_maker.data.model.PartAPI
 import retrofit2.Response
 import retrofit2.http.GET
 interface ApiService {
-    @GET("/api/ST181_HalloweenMaker")
+    @GET("/api/ST169_AnimalOcMaker")
     suspend fun getAllData(): Response<Map<String, List<PartAPI>>>
 }
--- app/src/main/java/com/example/st181_halloween_maker/core/utils/key/DomainKey.kt
+++ app/src/main/java/com/example/st181_halloween_maker/core/utils/key/DomainKey.kt
@@ -1,9 +1,9 @@
 package com.example.st181_halloween_maker.core.utils.key
 
 object DomainKey {
-    const val BASE_URL = "https://lvtglobal.tech"
-    const val BASE_URL_PREVENTIVE = "https://lvtglobal.size"
-    const val SUB_DOMAIN = "/public/app/ST181_HalloweenMaker"
+    const val BASE_URL = "https://lvtglobal.site"
+    const val BASE_URL_PREVENTIVE = "https://lvtglobal.tech"
+    const val SUB_DOMAIN = "/public/app/ST169_AnimalOcMaker"
     const val HTTP = "https://"
     const val AVATAR_CHARACTER_API = "avatar.png"
     const val LAYER_EXTENSION = ".png"
