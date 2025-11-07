238bb8f01de22966fab421ad4eee8863602278d8
thequyenle <quyenle26082001@gmail.com>
2025-11-08 03:58:39 +0700

    setupGanAds

--- app/src/main/java/com/animal/avatar/charactor/maker/App.kt
+++ app/src/main/java/com/animal/avatar/charactor/maker/App.kt
@@ -2,19 +2,29 @@ package com.animal.avatar.charactor.maker
 
 import android.app.Application
 import android.content.Context
+import com.animal.avatar.charactor.maker.ui.splash.SplashActivity
+import com.lvt.ads.util.AdsApplication
+import com.lvt.ads.util.AppOpenManager
 
-class App : Application() {
-
-    companion object {
-        lateinit var instance: App
-            private set
-
-        val context: Context
-            get() = instance.applicationContext
-    }
-
+class App : AdsApplication() {
     override fun onCreate() {
         super.onCreate()
-        instance = this
+        AppOpenManager.getInstance().disableAppResumeWithActivity(SplashActivity::class.java)
+    }
+
+    override fun enableAdsResume(): Boolean {
+        return true
+    }
+
+    override fun getListTestDeviceId(): MutableList<String>? {
+        return null
+    }
+
+    override fun getResumeAdId(): String {
+        return getString(R.string.open_resume)
+    }
+
+    override fun buildDebug(): Boolean {
+        return true
     }
 }
